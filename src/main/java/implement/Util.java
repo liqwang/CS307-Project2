214650c1bce457ae5e0fbad819f991e931ef0817
QuanQuan <12011619@mail.sustech.edu.cn>
2021-12-05 18:25:04 +0800

    完成通用CRUD的封装

--- src/main/java/implement/Util.java
+++ src/main/java/implement/Util.java
@@ -1,10 +1,13 @@
 package implement;
 
+import cn.edu.sustech.cs307.exception.EntityNotFoundException;
 import cn.edu.sustech.cs307.exception.IntegrityViolationException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Util {
     /**
@@ -38,11 +41,47 @@ public class Util {
                 e.printStackTrace();
                 throw new IntegrityViolationException();
             }
-            return ps.getGeneratedKeys().getInt(1);
+            ResultSet rs = ps.getGeneratedKeys();
+            rs.next();
+            return ps.getGeneratedKeys().getInt("id");
         } catch (SQLException e) {
             e.printStackTrace();
             System.exit(1);
             return -1;
+        }
+    }
+
+    /**
+     * 通用的查询操作，注意：参数顺序要按照构造器的参数顺序
+     */
+    public static <T> List<T> query(Class<T> clazz,Connection con,String sql,Object... param){
+        try(PreparedStatement ps=con.prepareStatement(sql)){
+            for (int i = 0; i < param.length; i++) {
+                ps.setObject(i+1,param[i]);
+            }
+            ResultSet rs;
+            try {
+                rs = ps.executeQuery();
+            }catch (SQLException e){
+                e.printStackTrace();
+                throw new EntityNotFoundException();
+            }
+            int col = rs.getMetaData().getColumnCount();
+            Constructor<T> constr = (Constructor<T>)clazz.getDeclaredConstructors()[0];
+            List<T> list = new ArrayList<>();
+            while(rs.next()){
+                ArrayList<Object> consParams = new ArrayList<>();
+                for (int i = 0; i < col; i++) {
+                    consParams.add(rs.getObject(i+1));
+                }
+                list.add(constr.newInstance(consParams));
+            }
+            rs.close();
+            return list;
+        } catch (InstantiationException|IllegalAccessException|InvocationTargetException|SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+            return null;
         }
     }
 }
